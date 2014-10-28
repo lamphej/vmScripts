398f28d50f9d2890abfb8f3e5379ea245860690c
James Lamphere <jim@gmh-enterprises.com>
2014-10-28 19:41:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# vmScripts
+
+This is my repository for different scripts/etc that I come up with pertaining to VMware and all their awesome products.
+  
+  resolution_set.py - edit .vmx file over ssh to allow different resolutions in guest
---
+++ resolution_set.py
@@ -0,0 +1,40 @@
+"""resolution_set
+
+Usage:
+  resolution_set.py <vmxpath> <X> <Y>
+
+  vmxpath: "[datastore2] Server 2012 Essentials/Server 2012 Essentials.vmx"
+  environment variables: ESXI_HOST, ESXI_USER, ESXI_PASS
+"""
+from docopt import docopt
+import sys
+import paramiko
+from os import environ as os_env
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='resolution_set')
+
+    x_res = int(sys.argv[2])
+    y_res = int(sys.argv[3])
+    vRamSize = x_res * y_res * 4
+
+    vmx_path = sys.argv[1]
+    config_dir = "/vmfs/volumes/" + vmx_path.split('[')[1].split(']')[0].lower() + "/"
+    config_dir += vmx_path.split('] ')[1].split('/')[0]
+    config_file = vmx_path.split('/')[-1]
+    print "[+] Editing '%s' via ssh" % config_file
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(os_env['ESXI_HOST'], username=os_env['ESXI_USER'], password=os_env['ESXI_PASS'])
+    commands = [
+        "cd %s" % config_dir,
+        "echo %s >> %s" % ('svga.autodetect = "FALSE"', config_file),
+        "echo %s >> %s" % ('svga.vramSize = %s' % vRamSize, config_file),
+        "echo %s >> %s" % ('svga.maxWidth = %s' % x_res, config_file),
+        "echo %s >> %s" % ('svga.maxHeight = %s' % y_res, config_file)
+    ]
+    #paramiko doesn't "save state", so you can't cd in one command,
+    #then expect to use the files from that dir in the next one
+    full_command = '; '.join(commands)
+    ssh.exec_command(full_command)
+    print "[+] All Done"
